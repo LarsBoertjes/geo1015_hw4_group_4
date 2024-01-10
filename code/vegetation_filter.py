0576f50918b96bed737d6e1472dbdeb1a5a51460
Noah <noahalting@gmail.com>
2024-01-10 19:01:46 +0100

    changed vegetation_filter to an executable python file
    Arguments to pass:  input.las output.las

--- code/vegetation_filter.py
+++ code/vegetation_filter.py
@@ -1,32 +1,33 @@
+import sys
 import laspy
 import numpy as np
 
-las = laspy.read('data\cropped_file.las')
+def filter_vegetation(input_file, output_file):
+    las = laspy.read(input_file)
 
-unclassified_points = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
-unclassified_points.points = las.points[las.classification == 1]
+    unclassified_points = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
+    unclassified_points.points = las.points[las.classification == 1]
 
-points_with_one_return = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
-points_with_one_return.points = las.points[las.number_of_returns == 1]
+    points_with_one_return = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
+    points_with_one_return.points = las.points[las.number_of_returns == 1]
 
-# Extract point positions as a NumPy array
-# points1 = np.vstack((unclassified_points.x, unclassified_points.y, unclassified_points.z)).T
-# points2 = np.vstack((points_with_one_return.x, points_with_one_return.y, points_with_one_return.z)).T
+    # use set operations on coordinates
+    common_points_indices = np.setdiff1d(np.where(las.classification == 1)[0], np.where(las.number_of_returns == 1)[0])
 
-#use set operations on coordinates
-common_points_indices = np.setdiff1d(np.where(las.classification == 1)[0], np.where(las.number_of_returns == 1)[0])
+    # Create a new LAS file with the common points
+    result = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
+    result.points = las.points[common_points_indices]
 
-# Create a new LAS file with the common points
-result = laspy.create(point_format=las.header.point_format, file_version=las.header.version)
-result.points = las.points[common_points_indices]
+    result.write(output_file)
 
-result.write('unclassified_points_with_one_return.las')
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python vegetation_filter.py input.las output.las")
+        sys.exit(1)
 
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
+    filter_vegetation(input_file, output_file)
 
-
-# classification = point.array['classification']
-# in array the following can be extracted (according to point format 0 in documentation:
-# intensity, return_number, number_of_returns, scan_direction_flag, edge_of_flight_line, classification,
-# synthetic, key_point, withheld, scan_angle_rank, user_data, point_source_id
-
+    print(f'filtered pointcloud written to {output_file}')
