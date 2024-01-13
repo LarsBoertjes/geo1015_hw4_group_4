de7973c252e345fe629b061e31f187d3fa3c062d
LarsBoertjes <120394687+LarsBoertjes@users.noreply.github.com>
2024-01-13 10:21:43 +0100

    Updated main.cpp
    - finished laplace interpolation inclusive handling of nodata points

--- code/main.cpp
+++ code/main.cpp
@@ -30,22 +30,23 @@ typedef Delaunay::Face_handle Face_handle;
 #include <lasreader.hpp>
 #include <laswriter.hpp>
 
-
 #include "DatasetASC.h"
 
 //-- forward declarations of all functions
 std::vector<Point3> read_lasfile(std::string filename, int thin = 1);
 void write_obj(Delaunay &dt);
-void write_lasfile(const char filename[256], std::vector<Point3> points);
 std::vector<Point3> extractLowestPtsInGrid (const std::vector<Point3>& points, int gridRows, int gridCols, double min_x, double max_x, double min_y, double max_y);
 double barycentricInterpolation(double x, double y, std::vector<double>& x_coord, std::vector<double>& y_coord, std::vector<double>& z_vals);
 double maxAngleDegrees(double x, double y, double z, std::vector<double>& x_coord, std::vector<double>& y_coord, std::vector<double>& z_vals);
 void laplaceInterpolation(DatasetASC &dtm, const std::vector<Point3> &groundPts, Delaunay dt);                                
+Point2 calculateCircumcenter(float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, float p3_y);
+double computeDistance(float Ax, float Ay, float Bx, float By);
 
 int main(int argc, char** argv)
 {
   // read pointcloud from input file
-  std::vector<Point3> lsPts = read_lasfile("../data/69BZ2_13.LAZ", 1000);
+  std::vector<Point3> lsPts = read_lasfile("../data/69BZ2_13.LAZ", 10);
+  //std::vector<Point3> croppedData = read_lasfile("../data/")
 
   // specify the bounding box of 500x500 meters
   double min_x = 187465.5;
@@ -53,22 +54,16 @@ int main(int argc, char** argv)
   double min_y = 315228.5;
   double max_y = min_y + 500;
 
-  // extract points within bounding box
+  // extract points within bounding box + margin
   std::vector<Point3> croppedPts;
   for (auto pt : lsPts) {
-    if (pt.x() >= min_x && pt.x() <= max_x && pt.y() >= min_y && pt.y() <= max_y) {
+    if (pt.x() >= min_x - 25 && pt.x() <= max_x + 25 && pt.y() >= min_y - 25 && pt.y() <= max_y + 25) {
       croppedPts.push_back(pt);
     }
   }
 
   // Step 0 of Ground filtering with TIN refinement: create array with locally lowest points in 2D grid
-  // -- The size of the largest building is around 23x20 meters so we will use a grid resolution of almost 30x30m (500x500 / 17 rows, columns = 29.4 x 29.4)
-  // -- Divide the 500x500m into a grid with 17 columns and rows and per cell push the cell with the lowest pt.z() value to groundPts
-  std::vector<Point3> groundPts = extractLowestPtsInGrid(croppedPts, 17, 17, min_x, max_x, min_y, max_y);
-
-  std::cout << "Number of groundPts before adding: " << groundPts.size() << std::endl;
-  std::cout << "Number of croppedPts before adding: " << croppedPts.size() << std::endl; 
-  std::cout << std::endl;
+  std::vector<Point3> groundPts = extractLowestPtsInGrid(croppedPts, 20, 20, min_x - 25, max_x + 25, min_y - 25, max_y + 25);
 
   // Step 1 of Ground filtering with TIN refinement: Construction of a rudimentary initial TIN
   Delaunay dt;
@@ -80,8 +75,8 @@ int main(int argc, char** argv)
 
   // Step 2: Computation of two geometric properties for each point that is not already labelled as ground
   // push all new ground points to groundPts vector
-  double d_max = 3;
-  double alpha_max = 5;
+  double d_max = 2;
+  double alpha_max = 3;
 
   for (int i = 0; i < croppedPts.size(); ++i) {
     // check if pt is not already in groundPts
@@ -118,6 +113,8 @@ int main(int argc, char** argv)
 
       if (maxAngle < alpha_max && heightDifference < d_max) {
         groundPts.push_back(croppedPts[i]);
+        vh = dt.insert(Point2(croppedPts[i].x(), croppedPts[i].y()));
+        vh->info() = croppedPts[i].z();
       }
     }
   }
@@ -128,31 +125,17 @@ int main(int argc, char** argv)
   // Please perform the laPlace Interpolation here and create a gridded DTM 50cmX50cm
 
   DatasetASC d = DatasetASC(1000, 1000, min_x, min_y, 0.5, -9999.0);
-  
-  //-- you could modify it
-  //d.data[1][2] = 666.666;
-  //-- row-col => x-y
-  //double x, y;
-  //d.rc2xy(2, 0, x, y);
-  //std::cout << "(" << x << ", " << y << ")" << std::endl;
-  //-- x-y => row-col
-  //int r, c;
-  //d.xy2rc(138, 122, r, c);
-  //std::cout << "(" << r << ", " << c << ")" << std::endl;
-
-  //-- write the array to a ASC file 
-  //d.write("out.asc"); 
 
   laplaceInterpolation(d, groundPts, dt);
 
-  std::cout << groundPts[0] << " " << groundPts[1] << " " << groundPts[2] << std::endl;
-
   // Write the gridded DTM to an ASC file
   d.write("../data/dtm.asc");
+
+  std::cout << d.data[119][648] << std::endl;
+  std::cout << d.data[d._nrows - 119 - 1][648] << std::endl;
     
   //-- we're done, return 0 to say all went fine
   return 0;
-
 
 }
 
@@ -296,19 +279,117 @@ double maxAngleDegrees(double x, double y, double z, std::vector<double>& x_coor
 }
 
 void laplaceInterpolation(DatasetASC &dtm, const std::vector<Point3> &groundPts, Delaunay dt) {
-    // Iterate over each cell in the DTM
-    int count = 0;
+  std::vector<std::vector<int>> nanCells = {};
 
-    for (int row = 0; row < dtm._nrows && count < 25; ++row) {
-        for (int col = 0; col < dtm._ncols && count < 25; ++col) {
-          double cell_center_x = static_cast<double>(dtm._xllcorner + (dtm._cellsize * col) + (dtm._cellsize / 2.0));
-          double cell_center_y = dtm._yllcorner + (dtm._cellsize * row) + (static_cast<double>(dtm._cellsize) / 2.0);
-          std::cout << row << " " << col << " has cell center: (" << cell_center_x << "," << cell_center_y << ")" << std::endl;        
-          Point2 pt_xy(cell_center_x, cell_center_y);
-          Face_handle fh = dt.locate(pt_xy);
-          std::cout << "and it is located in triangle with vertex0: " << fh->vertex(0)->point() << std::endl;
-          count++;    
+  for (int row = 0; row < dtm._nrows; ++row) {
+    for (int col = 0; col < dtm._ncols; ++col) {
+
+      // get coordinates of the center of the cells
+      double cell_center_x = dtm._xllcorner + (dtm._cellsize * col) + (dtm._cellsize / 2.0);
+      double cell_center_y = dtm._yllcorner + (dtm._cellsize * row) + (dtm._cellsize / 2.0);
+      Point2 cell_center(cell_center_x, cell_center_y);  
+      
+      // insert vertex_cell_center into DT and iterate over incident vertices
+      Delaunay::Vertex_handle vertex_cell_center = dt.insert(cell_center);
+      Delaunay::Vertex_circulator vc = dt.incident_vertices(vertex_cell_center);
+      Delaunay::Vertex_circulator done = vc;
+
+      // circulate over incident faces of vertex_cell_center
+      Delaunay::Face_circulator fc = dt.incident_faces(vertex_cell_center);
+      Delaunay::Face_circulator done_face = fc;
+
+      int incidentVertexNum = 0;
+      int incidentFaceNum = 0;          
+          
+      double sum_weighted_height = 0;
+      double total_weight = 0;
+
+      if (vc != 0 && fc != 0) {
+        do {
+          // iterate over the faces from face_circulator fc 
+          Delaunay::Vertex_handle vh1 = fc->vertex(0);
+          Delaunay::Vertex_handle vh2 = fc->vertex(1);
+          Delaunay::Vertex_handle vh3 = fc->vertex(2);
+
+          std::vector<Delaunay::Vertex_handle> secondTri;
+
+          for (int i = 0; i < 3; ++i) {
+            std::vector<Point2> faceVertices = {};
+            Delaunay::Vertex_handle uh1 = fc->neighbor(i)->vertex(0);
+            Delaunay::Vertex_handle uh2 = fc->neighbor(i)->vertex(1);
+            Delaunay::Vertex_handle uh3 = fc->neighbor(i)->vertex(2);
+            faceVertices.push_back(uh1->point());
+            faceVertices.push_back(uh2->point());
+            faceVertices.push_back(uh3->point());
+
+            // check if cell center & vc->point() are in faceVertices
+            if (std::find(faceVertices.begin(), faceVertices.end(), cell_center) != faceVertices.end() &&
+                std::find(faceVertices.begin(), faceVertices.end(), vc->point()) != faceVertices.end()) {
+                    secondTri.push_back(uh1);
+                    secondTri.push_back(uh2);
+                    secondTri.push_back(uh3);
+                  } 
+                } 
+
+            Point2 u1 = secondTri[0]->point();
+            Point2 u2 = secondTri[1]->point();
+            Point2 u3 = secondTri[2]->point();
+
+            Point2 circumcenter_a = circumcenter(vh1->point(), vh2->point(), vh3->point());
+            Point2 circumcenter_b = circumcenter(u1, u2, u3);
+
+            double edgeVx = computeDistance(circumcenter_a.x(), circumcenter_a.y(), circumcenter_b.x(), circumcenter_b.y());
+            double xpi = computeDistance(vertex_cell_center->point().x(), vertex_cell_center->point().y(), vc->point().x(), vc->point().y());
+
+            double weight_i = edgeVx / xpi;
+            double weighted_height = weight_i * vc->info();
+
+            sum_weighted_height += weighted_height;
+            total_weight += weight_i;
+            
+            ++vc; 
+            ++fc;
+            } while (vc != done);
+          }
+
+          double interpolated_z = sum_weighted_height / total_weight;
+
+          // Handling of nodata, since getting the nearest vertex will give us a nan value we store it here in an array
+          // and iterate over it again at the end and get the average values from neighboring cells
+          if (std::isnan(interpolated_z)) {
+            nanCells.push_back({row, col});
+          }
+
+          // remove vertex_cell_center from DT and proceed
+          dt.remove(vertex_cell_center);
+
+          // add interpolated z value to the empty grid
+          dtm.data[dtm._nrows - row - 1][col] = interpolated_z;
         }
+    }
+
+    // filling nodata points using average of 4 adjacent cells
+    for (int i = 0; i < nanCells.size(); ++i) {
+      int row = nanCells[i][0];
+      int col = nanCells[i][1];
+
+      double sumHeightNeighbors = 0;
+
+      sumHeightNeighbors += dtm.data[dtm._nrows - row - 1][col + 1];
+      sumHeightNeighbors += dtm.data[dtm._nrows - row - 1][col - 1];
+      sumHeightNeighbors += dtm.data[dtm._nrows - row][col];
+      sumHeightNeighbors += dtm.data[dtm._nrows - row - 2][col];
+
+      dtm.data[dtm._nrows - row - 1][col] = sumHeightNeighbors / 4;
+
     }
 }
 
+double computeDistance(float Ax, float Ay, float Bx, float By) {
+  double x_diff = abs(Ax - Bx);
+  double y_diff = abs(Ay - By);
+
+  double distance = sqrt((x_diff * x_diff) + (y_diff * y_diff));
+
+  return distance;
+}
